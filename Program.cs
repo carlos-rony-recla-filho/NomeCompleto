7049430356e045e7c61ed4a789490509bfc7d19b
Carlos Rony <carlosrecla03@gmail.com>
2021-09-10 16:45:20 -0300

    finalização da tarefa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome;
+
+            Console.Write("Digite seu nome......:");
+            nome = Console.ReadLine();
+            Console.Write("Digite seu sobrenome.:");
+            sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"{nome} {sobrenome}");
+            Console.WriteLine($"{sobrenome.ToUpper()}, {nome}");
+        }
+    }
+}
